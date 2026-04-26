2b4f57623878ce43e3372a3a6b9632280241ca5a
Precious <toddhurray@gmail.com>
2026-04-26 01:16:13 +0100

    Update service-worker.js
    Signed-off-by: Precious  <toddhurray@gmail.com>

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,17 @@
-const CACHE_NAME = 'calculator-v2'; // Changed version number
-
-const urlsToCache = [
-  '/Calculator-app/',
-  '/Calculator-app/index.html'
-];
-
+// Kill the old cache completely
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-  self.skipWaiting(); // Force update
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName); // Delete old caches
-          }
-        })
-      );
-    })
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => caches.delete(key))
+    )).then(() => self.clients.claim())
   );
 });
 
+// Don't cache anything for now - just fetch fresh
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
